c593616c59078fb242c9e52e756b155204b53930
Sebastian <sebas.a.diaz91@gmail.com>
2026-05-20 16:40:15 +0200

    fix: remove duplicate historyLoginRequired keys; split vendor chunks

--- construction-automation/app4-site-report/vite.config.js
+++ construction-automation/app4-site-report/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,6 +7,16 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-antd': ['antd', '@ant-design/icons'],
+        },
       },
     },
   },
